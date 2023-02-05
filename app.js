1863aa016ccedf9d801f9bb3ffebcc2f4ae25300
MohamedYahia831 <mohamedyahia207545@gmail.com>
2023-02-05 23:37:58 +0200

    start

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-	console.log('server is running on http://localhost:' + port);
+  console.log(`server is running on http://localhost:${port}`);
 });
