451b95b0498156ebddd2647814ebb9011245cdaa
yahia <mohamedyahia207545@gmail.com>
2023-02-07 22:46:14 +0200

    CRUD

--- app.js
+++ app.js
@@ -1,9 +1,51 @@
 const express = require("express");
 
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const dbconnection = require("./config/database");
+const userRoute = require("./routes/userRoute");
+const globalError = require("./middlewares/errorMiddleware");
+const ApiError = require("./utlis/apiError");
+// Connect with DB.
+dbconnection();
+
+// express app
 const app = express();
 
-const port = process.env.PORT || 3000;
+// Middlewares
+app.use(express.json());
 
-app.listen(port, () => {
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode: ${process.env.NODE_ENV}`);
+} else {
+  console.log(`mode: ${process.env.NODE_ENV}`);
+}
+
+// Mount Routers
+app.use("/api/iti/users", userRoute);
+
+app.all("*", (req, res, next) => {
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
+});
+
+// Global error handling middleware for express
+app.use(globalError);
+
+const port = process.env.PORT || 5000;
+
+const server = app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
+});
+
+// Handle rejection outside express
+process.on("unhandledRejection", (err) => {
+  console.error(`UnhandledRejection Errors: ${err.name} | ${err.message}}`);
+  server.close(() => {
+    console.log("Shutting down...");
+    process.exit(1);
+  });
 });
---
+++ routes/userRoute.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../services/userService");
+const {
+  getUserValidator,
+  createUserValidator,
+  updateUserValidator,
+  deleteUserValidator,
+} = require("../utlis/validators/userValidator");
+
+const router = express.Router();
+
+router.route("/").get(getUsers).post(createUserValidator, createUser);
+router
+  .route("/:id")
+  .get(getUserValidator, getUser)
+  .put(updateUserValidator, updateUser)
+  .delete(deleteUserValidator, deleteUser);
+
+module.exports = router;
