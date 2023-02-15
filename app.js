21367ceab052e89aaa1791d8fadf10677ebc743c
Fatma-Elsayed-Hussien <fatima126515@gmail.com>
2023-02-15 20:47:54 +0200

    update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./router/auth');
+const authRoutes = require('./router/authRoute');
 
 const app = express();
 require('dotenv').config();
@@ -10,18 +9,9 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'uploads'))); 
 app.use(authRoutes);
 
-mongoose.set('strictQuery', false);
-mongoose.connect('mongodb://127.0.0.1:27017/users').then(_ =>{
-	console.log('database connect successfully');
-});
-
-app.use(authRoutes);
-
-
 const port = process.env.PORT || 3000;
 
 app.use((err, req, res, next) => {
-	// console.log(err.message);
     if(!err.statusCode){
         err.message = "Something Went Wrong";
     }
--- router/authRoute.js
+++ router/authRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const { signup, login } = require('../controller/auth');
+const { signup, login } = require('../controller/authController');
 const { uploadSingleImage } = require('../middlewares/uploadImageMiddleware');
 const { signupValidator, loginValidator } = require('../utils/validators/authValidator');
-const authorizeUser = require('../middlewares/authorizeUserMiddleware');
+const authorizeUser = require('../middlewares/is-auth');
 
 const router = express.Router();
 
