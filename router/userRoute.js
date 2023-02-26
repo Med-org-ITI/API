27a1a522a85baf263d28dda3b14d533174c8e2d9
yahia <mohamedyahia207545>
2023-02-26 23:21:35 +0200

    updated

--- router/userRoute.js
+++ router/userRoute.js
@@ -31,61 +31,40 @@ const itemRoute = require('./itemRoute');
 
 router.use('/:userId/items', itemRoute);
 
-router.get('/getMe', authService.protect, getLoggedUserData, getUser);
-router.put(
-  '/changeMyPassword',
-  authService.protect,
-  updateLoggedUserPasswordValidator,
-  updateLoggedUserPassword
-);
-router.put(
-  '/updateMe',
-  authService.protect,
-  uploadUsreImage,
-  resizeImage,
-  updateLoggedUserDataValidator,
-  updateLoggedUserData
-);
-router.delete('/deleteMe', authService.protect, deleteLoggedUserData);
-
 router.put(
   '/changePassword/:id',
   changeUserPasswordValidator,
   changeUserPassword
 );
 
+router.app.use(itemRoute);
+router.use(authService.protect);
+
+router.get('/getMe', getLoggedUserData, getUser);
+router.put(
+  '/changeMyPassword',
+  updateLoggedUserPasswordValidator,
+  updateLoggedUserPassword
+);
+router.put(
+  '/updateMe',
+  uploadUsreImage,
+  resizeImage,
+  updateLoggedUserDataValidator,
+  updateLoggedUserData
+);
+router.delete('/deleteMe', deleteLoggedUserData);
+
+router.route('/').get(authService.allowedTo('admin', 'manager'), getUsers);
+
 router
-  .route('/')
-  .get(authService.protect, authService.allowedTo('admin', 'manager'), getUsers)
-  .post(
-    authService.protect,
-    authService.allowedTo('admin'),
-    uploadUsreImage,
-    resizeImage,
-    createUserValidator,
-    createUser
-  );
+  .use(authService.allowedTo('admin'))
+
+  .post(uploadUsreImage, resizeImage, createUserValidator, createUser);
 router
   .route('/:id')
-  .get(
-    authService.protect,
-    authService.allowedTo('admin'),
-    getUserValidator,
-    getUser
-  )
-  .put(
-    authService.protect,
-    authService.allowedTo('admin'),
-    uploadUsreImage,
-    resizeImage,
-    updateUserValidator,
-    updateUser
-  )
-  .delete(
-    authService.protect,
-    authService.allowedTo('admin'),
-    deleteUserValidator,
-    deleteUser
-  );
+  .get(getUserValidator, getUser)
+  .put(uploadUsreImage, resizeImage, updateUserValidator, updateUser)
+  .delete(deleteUserValidator, deleteUser);
 
 module.exports = router;
