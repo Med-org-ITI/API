5d644d6ed854c573daaf7d8765744601f5f3ada1
unknown <baselsalah2053@gmail.com>
2023-03-02 17:16:56 +0200

    orders error fix

--- controller/orderController.js
+++ controller/orderController.js
@@ -16,7 +16,7 @@ exports.getOrder = asyncHandler(async (req, res) => {
 // HERE  User
 exports.userOrders = asyncHandler(async (req, res, next) => {
 	if (req.user._id !== req.params.id) {
-		return next(new ApiError(401, 'You are not allowed to get this information'));
+		return next(new ApiError('You are not allowed to get this information', 401));
 	}
 	const orders = await Order.find({ userId: req.params.id });
 	res.status(200).json({ message: 'got user orders', orders });
@@ -24,21 +24,21 @@ exports.userOrders = asyncHandler(async (req, res, next) => {
 
 exports.userCompletedOrders = asyncHandler(async (req, res, next) => {
 	if (req.user._id !== req.params.id) {
-		return next(new ApiError(401, 'You are not allowed to get this information'));
+		return next(new ApiError('You are not allowed to get this information', 401));
 	}
 	const orders = await Order.find({ status: 'completed', userId: req.params.id });
 	res.status(200).json({ message: 'got user completed orders', orders });
 });
 
-exports.createOrder = asyncHandler(async (req, res) => {
+exports.createOrder = asyncHandler(async (req, res, next) => {
 	const cart = await Cart.findOne({ userId: req.user._id });
-	if (cart.items.length > 0) {
-		const order = new Order({ total: cart.total, items: cart.items, userId: cart.userId });
-		cart.items = [];
-		cart.total = 0;
-		await cart.save();
-		await order.save();
-		return res.status(201).json('order created');
+	if (cart.items.length <= 0) {
+		return next(new ApiError('cart must be filled with items', 422));
 	}
+	const order = new Order({ total: cart.total, items: cart.items, userId: cart.userId });
+	cart.items = [];
+	cart.total = 0;
+	await cart.save();
+	await order.save();
 	res.status(422).json('cart must be filled');
 });
