8643f544700d37992d8c52f3183ee8cceb406ca7
unknown <baselsalah2053@gmail.com>
2023-03-03 10:23:33 +0200

    change db link

--- config/database.js
+++ config/database.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
 
 const dbConnection = () => {
-	// mongoose.connect(process.env.MONGODB_URI).then(async con => {
-	// 	console.log(`connect successfuly at : ${con.connection.host}`);
-	// });
-	mongoose.connect('mongodb://127.0.0.1:27017/med-angular').then(async con => {
+	mongoose.connect(process.env.MONGODB_URI).then(async con => {
 		console.log(`connect successfuly at : ${con.connection.host}`);
 	});
+	// mongoose.connect('mongodb://127.0.0.1:27017/med-angular').then(async con => {
+	// 	console.log(`connect successfuly at : ${con.connection.host}`);
+	// });
 };
 
 module.exports = dbConnection;
